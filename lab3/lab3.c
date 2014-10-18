e3f7bfdd7c7173e13f7abc46489e615b22de0ba7
up201303313 <up201303313@5e6739d0-68c5-4bdb-a525-a8c7b29f8cc5>
2014-10-18 10:13:57 +0000

    Started lab3

---
+++ lab3/lab3.c
@@ -0,0 +1,6 @@
+#include "test3.h"
+
+int main()
+{
+	return 0;
+}
--- lab3/test3.c
+++ lab3/test3.c
@@ -1,9 +1,18 @@
-int kbd_test_scan(unsigned short ass); {
+#include "test3.h"
+
+int kbd_test_scan(unsigned short ass)
+{
 	/* To be completed */
 }
-int kbd_test_leds(unsigned short n, unsigned short *leds) {
+
+
+int kbd_test_leds(unsigned short n, unsigned short *leds)
+{
     /* To be completed */
 }
-int kbd_test_timed_scan(unsigned short n) {
+
+
+int kbd_test_timed_scan(unsigned short n)
+{
     /* To be completed */
 }
