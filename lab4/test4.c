7739c8bf7c377496f81b07c2c3335c41e88590f9
up201305980 <up201305980@5e6739d0-68c5-4bdb-a525-a8c7b29f8cc5>
2014-11-08 15:25:25 +0000

    Functions needed on this lab

---
+++ lab4/test4.c
@@ -0,0 +1,17 @@
+
+
+int test_packet(unsigned short cnt){
+    /* To be completed ... */
+}
+
+int test_async(unsigned short idle_time) {
+    /* To be completed ... */
+}
+
+int test_config(void) {
+    /* To be completed ... */
+}
+
+int test_gesture(short length, unsigned short tolerance) {
+    /* To be completed ... */
+}
