462cf37ec813c5a1b2d092994fc4a70a8b04bc4b
up201303313 <up201303313@5e6739d0-68c5-4bdb-a525-a8c7b29f8cc5>
2014-11-16 09:03:47 +0000

    Added corrected files. Had to do this because of svn problem

---
+++ lab5/lab5.c
@@ -0,0 +1,13 @@
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include <minix/com.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+        sef_startup();
+
+
+        return 0;
+}
